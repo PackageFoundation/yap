b2385717a06cf9225e738fd9ff0366b17559a552
M0Rf30 <morf3089@gmail.com>
2022-01-26 00:13:26 +0100

    chore(docker): update images
    chore(docker): remove build scripts
    
    chore(docker): hadolint validation

--- docker/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-
-for dir in */ ; do
-    cd $dir
-    sudo docker build --rm -t m0rf30/yap-${dir::-1} .
-    cd ..
-done
--- docker/push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-
-for dockerfile in $(ls Dockerfile.*) ; do
-    docker push m0rf30/yap-${dockerfile#Dockerfile.}:latest
-done
