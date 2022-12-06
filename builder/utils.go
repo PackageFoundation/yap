90f3458908f027a05d26f7b13e94487cbac905ff
M0Rf30 <morf3089@gmail.com>
2022-12-06 16:23:47 +0100

    chore: disable gosh interp
    although mvdan/interp is awesome, it's not so battle-tested as the sh shell

--- builder/utils.go
+++ builder/utils.go
@@ -1,25 +1,11 @@
 package builder
 
 import (
-	"context"
-	"os"
-	"strings"
-
-	"mvdan.cc/sh/v3/expand"
-	"mvdan.cc/sh/v3/interp"
-	"mvdan.cc/sh/v3/syntax"
+	"github.com/packagefoundation/yap/utils"
 )
 
 func runScript(cmds string) error {
-	buildScript, _ := syntax.NewParser().Parse(strings.NewReader(cmds), "")
-
-	runner, _ := interp.New(
-		interp.Env(expand.ListEnviron(os.Environ()...,
-		)),
-		interp.StdIO(nil, os.Stdout, os.Stdout),
-	)
-
-	err := runner.Run(context.TODO(), buildScript)
+	err := utils.Exec("", "sh", "-c", cmds)
 	if err != nil {
 		return err
 	}
--- go.mod
+++ go.mod
@@ -13,8 +13,5 @@ require (
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
-	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
